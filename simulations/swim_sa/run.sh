0e82558a52430a32628ef94ef222fef2dae1c4e3
Gabriel Moreno <gabrielm@cs.cmu.edu>
2018-01-17 09:17:39 -0500

    fix paths

--- simulations/swim_sa/run.sh
+++ simulations/swim_sa/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MAINSIMDIR=../../../plasasim/src/
+MAINSIMDIR=../../src/
 MAINSIMEXEC=swim
 
 RUNS=""
@@ -12,5 +12,5 @@ if [ "$3" != "" ]; then
     INIFILE="$3"
 fi
 
-opp_runall -j1 $MAINSIMDIR/$MAINSIMEXEC $INIFILE -u Cmdenv -c $1 -n ..:$MAINSIMDIR:../../../queueinglib:../../out/gcc-debug:../../src -lqueueinglib $RUNS
+opp_runall -j1 $MAINSIMDIR/$MAINSIMEXEC $INIFILE -u Cmdenv -c $1 -n ..:$MAINSIMDIR:../../../queueinglib:../../src -lqueueinglib $RUNS
 
