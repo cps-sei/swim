cd985f61b56800b48cacbe348eb93adce283ac8a
Martin Pfannemüller <martin.pfannemueller@uni-mannheim.de>
2019-05-01 14:17:24 +0200

    Working simulation (fixes #1)

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -2,8 +2,8 @@
 CURDIR=`pwd`
 DIR=`dirname $0`
 cd $DIR/omnetpp-vnc
-docker build . -t gabrielmoreno/omnetpp-vnc:5.2.1
+docker build . -t martinpfannemueller/omnetpp-vnc:5.4.1
 cd ..
-docker build . -t gabrielmoreno/swim:1.0
+docker build . -t martinpfannemueller/swim:1.0
 cd $CURDIR
 
